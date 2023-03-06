8883eb6a72e9cbf39ae1f8c9c88753dcad74fa8c
tangjs <tangjs@bbg.com>
2023-03-06 19:14:08 +0800

    Initial commit

---
+++ src/main/java/com/tangjs/wordCount.java
@@ -0,0 +1,27 @@
+package com.tangjs;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class wordCount {
+    public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // 从文件读取数据
+        env.readTextFile("input/words.txt")
+                .flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1L));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG))
+        .groupBy(0).sum(1).print();
+
+    }
+
+}
